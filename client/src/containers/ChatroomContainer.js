0a5133f87957894d9cbeec6f4a3e0aec6ba0e062
YuanShiDong <yuan@webimpact.co.jp>
2018-01-18 13:44:02 +0900

    bindActionCreators will build fail

--- client/src/containers/ChatroomContainer.js
+++ client/src/containers/ChatroomContainer.js
@@ -2,7 +2,7 @@ import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
 import * as actions from '../action/actions';
 import Chatroom from '../components/Chatroom/Chatroom';
-import bindActionCreators from "redux/src/bindActionCreators";
+// import bindActionCreators from "redux/src/bindActionCreators";
 
 const mapStateToProps = state => {
   return {
