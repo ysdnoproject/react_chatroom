1ec92d3774bd500ff17f5d2d02b84b59043f6d40
YuanShiDong <yuanshidong19921230@gmail.com>
2018-01-18 13:36:46 +0900

    fix redux-devtool

--- client/src/index.js
+++ client/src/index.js
@@ -8,8 +8,15 @@ import messages from './reducers/MessageReducer'
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import SignIn from "./components/SignIn";
 import ChatroomContainer from "./containers/ChatroomContainer";
+import {devToolsEnhancer} from "redux-devtools-extension";
 
-let store = createStore(messages)
+// let store = createStore(messages)
+
+//only dev
+const store = createStore(messages, /* preloadedState, */ devToolsEnhancer(
+  // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+));
+
 //必须要div 否则 会报错(div总是会render /  应该用switch)
 ReactDOM.render(
   <Provider store={store}>
