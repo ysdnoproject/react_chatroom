f3fd0b76f9e7642ca309f9f3b2a6194d943156c8
YuanShiDong <yuanshidong19921230@gmail.com>
2018-01-18 11:46:54 +0900

    format code

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 // import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 import messages from './reducers/MessageReducer'
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import SignIn from "./components/SignIn";
@@ -17,7 +17,7 @@ ReactDOM.render(
     <BrowserRouter>
       <Switch>
         <Route path="/chat" component={ChatroomContainer}></Route>
-        <Route path="/" component={SignIn} />
+        <Route path="/" component={SignIn}/>
       </Switch>
     </BrowserRouter>
   </Provider>,
--- client/src/reducers/MessageReducer.js
+++ client/src/reducers/MessageReducer.js
@@ -4,15 +4,18 @@ import * as messageTypes from '../constants/MessageTypes'
 
 
 const initialState = {
-  messages: [{type: messageTypes.SYSTEM_MESSAGE,
-    text: 'Welcome to our chatting room!'}],
+  messages: [{
+    type: messageTypes.SYSTEM_MESSAGE,
+    text: 'Welcome to our chatting room!'
+  }],
 };
 
 export default function messages(state = initialState, action) {
   switch (action.type) {
     case actionTypes.RECEIVE_MESSAGE:
       const message = action.message;
-      return {...state,
+      return {
+        ...state,
         messages: [...state.messages, {
           type: messageTypes.USER_MESSAGE,
           text: message.text,
@@ -20,14 +23,16 @@ export default function messages(state = initialState, action) {
         }]
       }
     case actionTypes.USER_JOINED:
-      return {...state,
+      return {
+        ...state,
         messages: [...state.messages, {
           type: messageTypes.SYSTEM_MESSAGE,
           text: `${action.data.userName} joined! Now ${action.data.userNumber} participants.`
         }]
       }
     case actionTypes.USER_LEFT:
-      return {...state,
+      return {
+        ...state,
         messages: [...state.messages, {
           type: messageTypes.SYSTEM_MESSAGE,
           text: `${action.data.userName} left! Now ${action.data.userNumber} participants.`
