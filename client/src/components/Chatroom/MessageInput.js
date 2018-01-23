345a240be6a78aef5f411f3af055e0518cb50e5d
袁凯忻 <yuankaixin@Kisn.local>
2018-01-23 16:31:11 +0800

    Updata css
    Updata css

--- client/src/components/Chatroom/MessageInput.js
+++ client/src/components/Chatroom/MessageInput.js
@@ -10,17 +10,41 @@ export default class MessageInput extends Component {
       if (text.length > 0) {
         this.props.sendMessage(text);
         this.refs.input.value = '';
+      }else{
+        alert('请输入内容~');
+        return false;
       }
+    }
+  }
+
+  /**
+   * @description send mesaages
+   */
+  sendMsg(e){
+    e.preventDefault();
+    const text = document.getElementById('msg-txt').value;
+    if (text.length > 0) {
+      this.props.sendMessage(text);
+      this.refs.input.value = '';
+    }else{
+      alert('请输入内容~');
+      return false;
     }
   }
 
   render() {
     return (
-      <input className="input-message"
-             placeholder="Type here..."
-             autoFocus="true"
-             ref="input"
-             onKeyDown={(e) => this.handleKeyDown(e)}/>
+      <div className="sent-frame">
+        <input className="input-message"
+              id = 'msg-txt'
+              placeholder="Type here..."
+              autoFocus="true"
+              ref="input"
+              onKeyDown={(e) => this.handleKeyDown(e)}/>
+        <button className = "sentbtn"
+                onClick = {(e) => this.sendMsg(e)}>
+        发送</button>
+      </div>
     );
   }
 }
